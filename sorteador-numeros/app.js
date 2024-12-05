9c8ad402a6376dd409f7cfbfda0bed7f71e4fc3f
Raul Favin <raulfavin@hotmail.com>
2024-12-05 17:28:34 +0000

    finalizando alterar botao e pausa do loop

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -3,7 +3,11 @@ function sortear() {
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
-    
+    if(quantidade >(ate - de +1)) {
+        alert("A quantidade n pode ser maior que o outro burro")
+        return
+    }
+
     let sorteados = [];
     let numero;
    
@@ -11,9 +15,10 @@ function sortear() {
         numero = sorteador(de, ate)
 
         while(sorteados.includes(numero)){
-        numero = sorteador(de , ate);
+        numero = sorteador(de, ate);
         }
         sorteados.push(numero);
+        alterarBotao();
     }
 
     let resultado = document.getElementById ("resultado");
@@ -23,7 +28,27 @@ function sortear() {
 }
 
 function sorteador(min , max) {
-    return Math.floor(Math.random() * (max - min +1)) + min;
-    
+    return Math.floor(Math.random() * (max - min +1)) + min; 
 }
     
+function alterarBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+
+}   
+
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarBotao();
+}
+
