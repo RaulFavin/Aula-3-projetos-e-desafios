74e74e7e2d847166a1919fc7d60a8e4c683bd346
Raul Favin <raulfavin@hotmail.com>
2025-01-22 13:30:35 +0000

    carrinho

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,15 @@
+function adicionar() {
+    //recuperar valores, nome do produto, quantidade e valor
+    //calcular o preco, o subtotal
+    //add produto no carrinho
+    //atualizar o valor total da compra
+    let NomeProduto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+    alert(produto.value);
+    alert(quantidade.value);
+
+}
+
+function limpar() {
+    
+}
