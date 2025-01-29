71cf5e5edb06be902de2e31450c6dd3141f12637
Raul Favin <raulfavin@hotmail.com>
2025-01-29 09:50:19 +0000

    carrinho de compras

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,13 +1,21 @@
-function adicionar() {
-    //recuperar valores, nome do produto, quantidade e valor
-    //calcular o preco, o subtotal
-    //add produto no carrinho
-    //atualizar o valor total da compra
-    let NomeProduto = document.getElementById('produto');
-    let quantidade = document.getElementById('quantidade');
-    alert(produto.value);
-    alert(quantidade.value);
+let total = 0 ; 
 
+function adicionar() {
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];   
+    //let valorUnitario = produto.split('R$')[1];
+    let valorUnitario = parseInt(produto.split('R$')[1].replace(',', '.')); // Converte para número
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
+        <section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span>${nomeProduto}<span class="texto-azul">R$${preco.toFixed(2)}</span>
+        </section>
+      </section>`;
+    total = total + preco ;
+    
+  
 }
 
 function limpar() {
