512757b51ae02b570cbbf6728fefa3822deed2ca
Raul Favin <raulfavin@hotmail.com>
2025-02-11 21:36:53 +0000

    conversor-cels-fahren

---
+++ exer/celsius-fahrenheit/app.js
@@ -0,0 +1,31 @@
+let celsius = " ";
+let fahrenheit = "77";
+
+
+
+function celsiusToFahre(celsius) {
+    fahrenheit = celsius * (9/5) + 32;
+    console.log(fahrenheit);
+}
+//celsiusToFahre(25);
+
+function FahreTocelsius(fahrenheit) {
+    celsius = (fahrenheit - 32) * 5/9;
+    console.log(celsius)
+}
+
+//FahreTocelsius(77);
+
+function conversor() {
+    if (celsius == " ") {
+        celsius = (fahrenheit - 32) * 5/9;
+        console.log(celsius);
+    }else{
+        fahrenheit = celsius * (9/5) + 32;
+        console.log(fahrenheit);
+    }
+    
+}
+conversor();
+
+
